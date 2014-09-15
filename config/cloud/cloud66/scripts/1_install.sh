7bcbbbf5e833776d37c66641e9fa208a76f14ba4
Andy Henson <andrew.henson@foxsoft.net>
2014-09-15 23:53:30 +0100

    Add new required packages

--- config/cloud/cloud66/scripts/1_install.sh
+++ config/cloud/cloud66/scripts/1_install.sh
@@ -8,6 +8,8 @@ else
 	source /var/.cloud66_env
 	apt-get install libicu-dev -y
     apt-get install acl -y
+    apt-get install pkg-config -y
+    apt-get install cmake -y
     apt-get install -y python-software-properties
     yes | add-apt-repository ppa:git-core/ppa
     apt-get -y update
